008e4d92668f53c4717827eb253ad757e3b73080
lamtranduc <32539012+lam-TD@users.noreply.github.com>
2018-08-31 14:59:28 +0700

    config database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'sql3.freemysqlhosting.net',
+            'port' => '3306',
+            'database' => 'sql3254459',
+            'username' => 'sql3254459',
+            'password' => 'Ag8im9Z2L1',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
